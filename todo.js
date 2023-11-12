54f581fe8fd2ddc601b0cf9ef28f62176e8b3c93
Thewebsitedeveloper <thewebsitedeveloper1976@gmail.com>
2023-11-12 14:00:43 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,1 +1,22 @@
 
+let input = document.getElementById("inp");
+let taskshower = document.querySelector(".taskshower");
+
+function Add(){
+    if (inp.value =="") {
+        alert("Please Enter Your Task.")
+    }
+    else{
+        let newEle = document.createElement('ul');
+         newEle.innerHTML = `${inp.value}<i class="fa-solid fa-trash"></i>`;
+         taskshower.appendChild(newEle);
+         inp.value = "";
+         newEle.querySelector("i").addEventListener('click', remove);
+         function remove(){
+            newEle.remove()
+         }
+    }
+        
+}
+
+
